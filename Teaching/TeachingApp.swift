3465fe30cfeabbb6393a96bb5cabfe93cf2b1636
Ansheng Zhou <azhou@lcs.on.ca>
2024-07-07 11:35:09 +0800

    Initial Commit

---
+++ Teaching/TeachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachingApp.swift
+//  Teaching
+//
+//  Created by Ansheng Zhou on 2024-07-07.
+//
+
+import SwiftUI
+
+@main
+struct TeachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
